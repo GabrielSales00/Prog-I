f5df3cb197ff06bbf1cd12fbc274f26f1dc0c779
Rokkuman1 <rokkuman78@gmail.com>
2022-05-02 11:04:15 -0300

    Delete Aula 7-2 directory

--- Aula 7-2/Ex3 dúvida.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char c;
-    for (c == 32; c == 255; c++) {
-        printf("%c, %d, %x\n", c, c, c);
-    }
-    return 0;
-}
--- Aula 7-2/Ex3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char c;
-    for (c = 32; c >= 32 && c <= 255; c++) { //Observe que "=" é usado para colocar em uma variável um determinado valor (ex: A = B + C é jogar o valor de B + C no endereço de A), enquanto "=="" checa se o valor de dois operandos são verdadeiros, por isso o lado esquerdo não deve ser uma constante.
-        printf("%d, %x\n", c, c);
-    }
-    return 0;
-}
