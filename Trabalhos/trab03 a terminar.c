c0580245f96a22800393b001b46e1e33256edc44
Gabriel Sales <rokkuman78@gmail.com>
2022-05-22 19:57:41 -0300

    Add files via upload

---
+++ Trabalhos/trab03 a terminar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*int main()
+{
+    int x;
+    struct peso
+    {
+        int knots;
+        int m[x][x];
+    } loc;
+    scanf("%d", x);
+    scanf("")
+    return 0;
+}
