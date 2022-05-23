5a2524d4c72f8dd75c3349e4df8eb1f9349a0e23
Gabriel Sales <rokkuman78@gmail.com>
2022-05-23 20:56:43 -0300

    Add files via upload

---
+++ Aula 13 - Modularização de prog I/Exemplo de função.c
@@ -0,0 +1,12 @@
+/*#include <stdio.h>
+void Ola() //Declaração da função: “(” “)” obrigatórios
+{
+    printf(“\n Ola!!! \n”); //Corpo da função
+}
+int main() { // Programa principal
+    printf(“Programa Principal!”);
+    Ola(); //Chamada da função
+    printf(“Posso chamar de novo:”);
+    Ola(); //Atenção: “(” “)” são obrigatórios!
+    return 0;
+}
