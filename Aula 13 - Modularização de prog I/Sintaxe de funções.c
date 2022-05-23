105cb8621617b156419562a8b0d0259cca959f4d
Gabriel Sales <rokkuman78@gmail.com>
2022-05-23 20:58:13 -0300

    Delete Aula 13 - Modularização de prog I directory

--- Aula 13 - Modularização de prog I/Sintaxe de funções.c
+++
@@ -1,9 +0,0 @@
-/*#include<stdio.h>
-
-<tipo_da_funcao> NomeDaFuncao(<lista_de_parametros>)
-{
-    instrucoes;
-}
-
-quando não for especificado o tipo, será interpretado como int;
-para testar o *procedimento*, use void, pois C permite apenas criar funções.
