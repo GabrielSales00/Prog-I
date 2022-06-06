4de3d71e11a5362e9521af1a9b1dc06fba3b8ccb
Gabriel Sales <rokkuman78@gmail.com>
2022-06-06 18:16:49 -0300

    Add files via upload

--- Aula 14 - Ponteiros/Ex3.c
+++ Aula 14 - Ponteiros/Ex3.c
@@ -7,20 +7,18 @@ void bubblesort(int x[N], int *sizePtr);
 int main() {
     int a[N], i, size;
     scanf("%d", &size);
-    for(i = 0; i < size - 1; i++) {
+    for(i = 0; i < size; i++) {
         scanf("%d", &a[i]);
     }
     bubblesort(a, &size);
-    for(i = 0; i < (size - 1); i++) {
-        printf("maior valor: %d \nmenor valor: %d\n", a[size-1], a);
-    }
+    printf("maior valor: %d\nmenor valor: %d\n", a[size - 1], a[0]);
     return 0;
 }
 
 void bubblesort(int x[N], int *sizePtr) {
     int j, i, hold;
-    for(j = 0; j < *sizePtr - 1; i++) {
-        for(i = 0; j < *sizePtr - 1; i++) {
+    for(j = 0; j < *sizePtr - 1; j++) {
+        for(i = 0; i < *sizePtr - 1; i++) {
             if(x[i] > x[i + 1]) {
                 hold = x[i];
                 x[i] = x[i + 1];
